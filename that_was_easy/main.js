49f4b7747362e199ec3d014fdcaeac9237237a71
Cody Armand <codyarmand@hotmail.com>
2016-10-10 20:29:22 +0000

    Final Sound

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -1,5 +1,7 @@
 function sayThatWasEasy() {
     var thatWasEasy = new Audio("that_was_easy.mp3"); 
     thatWasEasy.play(); 
-} 
-$("#easy").on("click", sayThatWasEasy); 
+}  
+
+$("#easy").on("click", sayThatWasEasy);  
+
