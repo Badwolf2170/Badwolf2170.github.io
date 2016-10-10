35f83def9a8ca856dccd63b472a54d6db4bda1b5
Cody Armand <codyarmand@hotmail.com>
2016-10-10 20:32:45 +0000

    Keyboard Press

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -5,3 +5,10 @@ function sayThatWasEasy() {
 
 $("#easy").on("click", sayThatWasEasy);  
 
+$(document).keypress(delegateKeypress); 
+
+function delegateKeypress(event) { 
+    if (event.keyCode == 32) { 
+        $("#easy").trigger("click"); 
+    } 
+}
