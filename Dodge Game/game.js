77edbb8a5c0645a45ed5406afd8d7ce25793ebbe
Cody Armand <codyarmand@hotmail.com>
2016-11-08 00:58:13 +0000

    Finished

--- Dodge Game/game.js
+++ Dodge Game/game.js
@@ -1,47 +1,50 @@
 var isGameOver;
 var player; 
+var playerImage;
 var enemy; 
+var enemyImage; 
+var backgroundImage;
 
+function preload() { 
+    playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png"); 
+    playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png"); 
+    backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png"); 
+}
 
 function setup() {
     isGameOver = false;
-    createCanvas(250, 250); 
-    player = createSprite(width/2, height-25, 50, 50)
-    enemy = createSprite(width/2, 0, 10, 30)
+    createCanvas(256, 256); 
+    player = createSprite(width/2, height-(playerImage.height/2), 0, 0); 
+    player.addImage(playerImage);
+    enemy = createSprite(width/2, 0, 0, 0); 
+    enemy.addImage(enemyImage); 
+    enemy.rotationSpeed = 4.0;
 } 
 
 function draw() { 
     if (isGameOver) { 
         gameOver();
-    if (enemy.overlap(player)) { 
-        gameOver(); 
     } else { 
         if (enemy.overlap(player)) { 
             isGameOver = true; 
-        
         }
+        if (enemy.overlap(player)) {
+           gameOver(); 
+        }
+        background(backgroundImage);
+        if (keyDown(RIGHT_ARROW) && player.position.x < (width-(playerImage.width/2))) {
+            player.position.x += 2
+        }    
+        if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width/2)) { 
+            player.position.x -= 2;
         } 
+        enemy.position.y = enemy.position + 3;  
+        if (enemy.position.y > height) { 
+            enemy.position.y = 0; 
+            enemy.position.x = random(5, width-5); 
+        }
+        drawSprites(); 
     }
-    
-    background(0, 0, 100); 
-    
-    
-    if (keyDown(RIGHT_ARROW) && player.position.x < width-25) {
-    player.position.x = player.position.x + 1;
-    }    
-
-    if (keyDown(LEFT_ARROW) && player.position.x > 25) { 
-        player.position.x = player.position.x - 1;
-    } 
-    
-    enemy.position.y = enemy.position + 3;  
-    
-    if (enemy.position.y > height) { 
-        enemy.position.y = 0; 
-        enemy.position.x = random(5, width-5);
-    }
-    
-    drawSprites();
 } 
 
 function gameOver() { 
@@ -53,11 +56,9 @@ function gameOver() {
 } 
 
 function mouseClicked() { 
-  if (isGameOver) { 
     isGameOver = false;
     player.position.x = width/2; 
-    player.position.y = height-25; 
-    enemy.position.x = width/2 
+    player.position.y = height-(playerImage.height/2); 
+    enemy.position.x = width/2; 
     enemy.position.y = 0;
-  }
 }
